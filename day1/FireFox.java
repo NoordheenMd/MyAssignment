d28756ef85f0c74e9d6c5985460ca874d7a37334
NoordheenMd <163240909+NoordheenMd@users.noreply.github.com>
2024-03-12 23:49:17 -0700

    Add files via upload
    Day1

---
+++ day1/FireFox.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class FireFox {
+	public static void main(String[] args) {
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		int releaseYear=1998;
+		char browserLogo='f';
+		System.out.println("browserversion is:"+browserVersion);
+		System.out.println("browsername is:"+browserName);
+		System.out.println("is this visible:"+isVisible);
+		System.out.println("release year:"+releaseYear);
+		System.out.println("browser logo:"+browserLogo);
+	}
+
+}
